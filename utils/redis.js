9b5906679ee78ecc479d63948862d9ba2fb19d4a
Matrix30 <Matrixboy30@gmail.com>
2024-06-17 16:18:03 +0100

    finish task 0. Redis utils

---
+++ utils/redis.js
@@ -0,0 +1,62 @@
+/**
+ * program to handle redis connections
+ * contains the class redis
+ */
+
+const redis = require('redis'); // importing redis
+
+class RedisClient {
+  constructor() {
+    this.redis_client = redis.createClient({
+      host: 'localhost',
+      port: 6379,
+    }); // creating a redis client
+    this.redis_client.on('error', (err) => {
+      console.log('Error occured in redis', err);
+    });
+  }
+
+  isAlive() {
+    return this.redis_client.connected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.redis_client.get(key, (err, response) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(response);
+        }
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.redis_client.setex(key, duration, value, (err, response) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(response);
+        }
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.redis_client.del(key, (err, response) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(response);
+        }
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
